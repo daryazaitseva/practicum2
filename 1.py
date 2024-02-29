def9b3f5ed11a9448d5778884a6a9cdcd067d60b
daryazaitseva <zaytsevaart@gmail.com>
2024-02-29 11:28:42 +0700

    task

--- 1.py
+++ 1.py
@@ -1,3 +1,8 @@
 # Зайцева Дарья Практическая работа №2
 
-
+silveramount = 96
+goldamount = silveramount // 16
+silverprice = 48
+revenue = int(input('Введите общую` сумму выручки '))
+goldprice = (revenue - silveramount*silverprice) // goldamount
+print(goldprice)
---
+++ 4.py
@@ -0,0 +1,8 @@
+me = 1
+man = 1
+wifes = 7
+bags = 7
+cats = 7
+kittens = 7
+count = me + man + wifes + bags * wifes * cats + bags * wifes * cats * kittens
+print(count)
