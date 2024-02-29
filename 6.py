fab6e79943eaf6102e6973e8fd63ea708324c935
daryazaitseva <zaytsevaart@gmail.com>
2024-02-29 11:59:09 +0700

    last changes

--- 6.py
+++ 6.py
@@ -1,3 +1,3 @@
 weight = float(input('Введите значение веса в фунтах '))
 height = float(input('Введите значение роста в дюймах '))
-print('Ваш индекс массы тела составляет ', format(weight*703/height**2, ' .2f'))
+print('Ваш индекс массы тела составляет ', round(weight*703/height**2, 2))
